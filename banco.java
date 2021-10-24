17790f3213c2347e5336011a150d4acd81f96a28
edja67 <93060769+edja67@users.noreply.github.com>
2021-10-24 11:29:08 -0300

    avaliação 1 unidade POO

--- banco.java
+++ banco.java
@@ -1,7 +1,37 @@
-	private int numeroAgencia; 
-	private String nomeAgencia;
+package avaliacao1;
+
+public class banco { 
 	
+	private int numeroagencia;
+    private String nomeagencia;
+
+
+    public banco(int numeroagencia, String nomeagencia) {
+	this.numeroagencia = numeroagencia;
+	this.nomeagencia = nomeagencia;
 	
-	public Banco(int numeroAgencia, String nomeAgencia) {
-		this.numeroAgencia = numeroAgencia; 
-		this.nomeAgencia = nomeAgencia; 
+	}
+
+
+	public int getNumeroagencia() {
+		return numeroagencia;
+	}
+
+
+	public void setNumeroagencia(int numeroagencia) {
+		this.numeroagencia = numeroagencia;
+	}
+
+
+	public String getNomeagencia() {
+		return nomeagencia;
+	}
+
+
+	public void setNomeagencia(String nomeagencia) {
+		this.nomeagencia = nomeagencia;
+	}
+    
+    
+
+}
